a0c9ded8fc1ef8aca126f66a14f976bb617fd345
Admin <abc@lao.com>
2019-10-13 11:34:32 +0800

    第3次提交

--- src/main/java/com/lao/vuespring/beans/Dog.java
+++ src/main/java/com/lao/vuespring/beans/Dog.java
@@ -3,4 +3,13 @@ package com.lao.vuespring.beans;
 public class Dog {
     private String name;
     private int age;
+    private float weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
